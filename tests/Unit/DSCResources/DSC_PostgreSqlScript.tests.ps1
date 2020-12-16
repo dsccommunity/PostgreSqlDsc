c7f9808dac38b4038cac16867b544ba8a254d2d6
Ryan Christman <8922086+rchristman89@users.noreply.github.com>
2020-12-16 13:32:19 -0500

    Added Complete error message

--- tests/Unit/DSCResources/DSC_PostgreSqlScript.tests.ps1
+++ tests/Unit/DSCResources/DSC_PostgreSqlScript.tests.ps1
@@ -97,8 +97,9 @@ try
                 It 'Should throw when psql is not found' {
                     $invalidParams = $scriptParams.Clone()
                     $invalidParams.PsqlLocation = 'Z:\does-not-exist.exe'
+                    $errorMessage = "The term 'Z:\does-not-exist.exe' is not recognized as a name of a cmdlet, function, script file, or executable program. Check the spelling of the name, or if a path was included, verify that the path is correct and try again."
 
-                    {Set-TargetResource @invalidParams } | Should -Throw -ExpectedMessage "The term 'Z:\does-not-exist.exe' is not recognized as the name of a cmdlet, function, script file, or executable program."
+                    {Set-TargetResource @invalidParams } | Should -Throw -ExpectedMessage $errorMessage
                 }
 
                 It 'Should re-throw errors from psql' {
